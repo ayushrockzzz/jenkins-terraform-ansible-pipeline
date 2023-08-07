4112e0783d4522e53806a4eeedd34e80d8d7cc43
ayush.satyam@wipro.com <ayush.satyam@wipro.com>
2023-08-07 13:29:54 +0530

    changed the region and added my credentials for s3 backend

--- s3-backend/s3.tf
+++ s3-backend/s3.tf
@@ -5,10 +5,6 @@ resource "aws_s3_bucket" "s3_bucket" {
   tags = local.tags
 }
 
-resource "aws_s3_bucket_acl" "s3_bucket" {
-  bucket = aws_s3_bucket.s3_bucket.id
-  acl    = "private"
-}
 
 resource "aws_s3_bucket_versioning" "s3_bucket" {
   bucket = aws_s3_bucket.s3_bucket.id
