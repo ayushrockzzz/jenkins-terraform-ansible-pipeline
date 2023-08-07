55336f32765932f5024e753e8c1f2614824f82bd
ayush.satyam@wipro.com <ayush.satyam@wipro.com>
2023-08-07 14:13:17 +0530

    changed the main.tf file

--- backend.tf
+++ backend.tf
@@ -1,10 +1,10 @@
 terraform {
   backend "s3" {
-    bucket         = "dungpham-s3-backend"
-    key            = "dungpham"
+    bucket         = "automation-team-pranad-ayush-jayant-s3-backend"
+    key            = "automation-team"
     region         = "ap-south-1"
     encrypt        = true
-    role_arn       = "arn:aws:iam::952429021322:role/DungphamS3BackendRole"
-    dynamodb_table = "dungpham-s3-backend"
+    role_arn       = "arn:aws:iam::278745882559:role/Automation-Team-Pranad-Ayush-JayantS3BackendRole"
+    dynamodb_table = "automation-team-pranad-ayush-jayant-s3-backend"
   }
 }
